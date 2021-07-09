4de4621f5bd052223c3d44b47b1989b69c133fce
sarah-blazic <83601788+sarah-blazic@users.noreply.github.com>
2021-07-09 14:43:51 -0700

    new config

---
+++ examples/main.tf
@@ -0,0 +1,18 @@
+provider "panos" {
+  hostname = var.hostname
+  username = var.user
+  password = var.password
+}
+
+module "sec_prof" {
+  source = "sarah-blazic/sec_profiles"
+
+  #for yaml file "./examples/yaml/..."
+  antivirus_file     = "./files/json/antivirus.json"
+  file_blocking_file = "./files/json/file_blocking.json"
+  wildfire_file      = "./files/json/wildfire.json"
+  vulnerability_file = "./files/json/vulnerability.json"
+  spyware_file       = "./files/json/spyware.json"
+}
+
+
