451bfb9fa3923fb4d224f2473b8e5597c9cae0bc
FrankTheTank <franklin@bitsmasher.net>
2021-07-20 17:36:25 -0400

    update mod name

--- examples/main.tf
+++ examples/main.tf
@@ -6,15 +6,15 @@ provider "panos" {
 
 module "security-profiles" {
   source  = "sarah-blazic/security-profiles/panos"
-  version = "0.1.0"
+  version = "0.1.1"
 
   #for JSON files: try(jsondecode(file("<*.json>")), {})
   #for YAML files: try(yamldecode(file("<*.yaml>")), {})
-  antivirus_file     = try(jsondecode(file("/workspace/source/examples/files/json/antivirus.json")), {})
-  file_blocking_file = try(jsondecode(file("files/json/file_blocking.json")), {})
-  wildfire_file      = try(jsondecode(file("files/json/wildfire.json")), {})
-  vulnerability_file = try(jsondecode(file("files/json/vulnerability.json")), {})
-  spyware_file       = try(jsondecode(file("files/json/spyware.json")), {})
+  antivirus_file     = try(jsondecode(file("./files/json/antivirus.json")), {})
+  file_blocking_file = try(jsondecode(file("./files/json/file_blocking.json")), {})
+  wildfire_file      = try(jsondecode(file("./files/json/wildfire.json")), {})
+  vulnerability_file = try(jsondecode(file("./files/json/vulnerability.json")), {})
+  spyware_file       = try(jsondecode(file("./files/json/spyware.json")), {})
 }
 
 
