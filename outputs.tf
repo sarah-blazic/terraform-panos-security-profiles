61681e04b1303e56befccfd694e416fbe7195112
sarah-blazic <83601788+sarah-blazic@users.noreply.github.com>
2021-07-26 09:53:45 -0700

    change input/var names

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,33 @@
 #antivirus
 output "created_antivirus_prof" {
   description = "Shows the antivirus security profiles that were created."
-  value       = var.antivirus_file != "optional" ? { for prof in var.antivirus_file : prof.name => prof } : tomap({})
+  value       = var.antivirus != "optional" ? { for prof in var.antivirus : prof.name => prof } : tomap({})
 }
 
 
 #anti-spyware
 output "created_spyware_prof" {
   description = "Shows the anti-spyware security profiles that were created."
-  value       = var.spyware_file != "optional" ? { for prof in var.spyware_file : prof.name => prof } : tomap({})
+  value       = var.spyware != "optional" ? { for prof in var.spyware : prof.name => prof } : tomap({})
 }
 
 
 #file blocking
 output "created_file_blocking_prof" {
   description = "Shows the file-blocking security profiles that were created."
-  value       = var.file_blocking_file != "optional" ? { for prof in var.file_blocking_file : prof.name => prof } : tomap({})
+  value       = var.file_blocking != "optional" ? { for prof in var.file_blocking : prof.name => prof } : tomap({})
 }
 
 
 #vulnerability
 output "created_vulnerability_prof" {
   description = "Shows the vulnerability security profiles that were created."
-  value       = var.vulnerability_file != "optional" ? { for prof in var.vulnerability_file : prof.name => prof } : tomap({})
+  value       = var.vulnerability != "optional" ? { for prof in var.vulnerability : prof.name => prof } : tomap({})
 }
 
 
 #wildfire analysis
 output "created_wildfire_prof" {
   description = "Shows the wildfire analysis security profiles that were created."
-  value       = var.wildfire_file != "optional" ? { for prof in var.wildfire_file : prof.name => prof } : tomap({})
+  value       = var.wildfire != "optional" ? { for prof in var.wildfire : prof.name => prof } : tomap({})
 }
